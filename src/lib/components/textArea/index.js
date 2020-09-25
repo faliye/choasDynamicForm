159e4c13e4ae467a8a78194adbf4aa7f72d0ec3c
fanye <1518878598@qq.com>
2020-09-25 09:49:10 +0800

    feat: 提交

--- src/lib/components/textArea/index.js
+++ src/lib/components/textArea/index.js
@@ -1,6 +1,7 @@
 import {$createElement as h} from '../../utils';
 import $ from 'jquery';
 import './inde.scss';
+import mainEvent from "../../mainEvent";
 
 /**
  * Input组件
@@ -17,12 +18,23 @@ export class TextArea {
   }
 
   render() {
+    const {location} = this.props;
+    const mainData = mainEvent.store.data[location[0]][location[1]].childrenProps;
+    console.log( mainData)
     this.$el = h('textarea',
         {
           className: ['component-textarea-box'],
           placeholder: '多行文本框',
-          value: this.props.value,
-        }
+          value: mainData.value,
+          on:{
+            input:(e)=>{
+              console.log(mainData.value)
+              mainData.value = e.target.value;
+              console.log(mainEvent.store.data)
+              mainEvent.emit('dataChange', mainEvent.store.data);
+            }
+          }
+        },
     )
   }
 }
